700122050530c8826e1cec7d2c3e616ae6d73018
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-06-05 19:44:04 -0400

    Clone of backend

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// Require controller modules.
+const weather_controller = require('../controllers/weatherController');
+
+// GET api basic info.
+router.get('/', weather_controller.index);
+
+// GET weather info.
+router.get('/weather', weather_controller.weather);
+
 
 module.exports = router;
